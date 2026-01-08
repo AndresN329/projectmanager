ca180d01641eed02d00922c3a60c69f1c4401bf4
AndresN329 <andresnieblesmartinez@gmail.com>
2026-01-08 11:22:48 -0500

    comit init

--- target/generated-sources/annotations/com/riwi/projectmanager/infrastructure/persistence/mapper/TaskMapperImpl.java
+++ target/generated-sources/annotations/com/riwi/projectmanager/infrastructure/persistence/mapper/TaskMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-07T23:07:40-0500",
+    date = "2026-01-08T10:49:44-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.9 (Eclipse Adoptium)"
 )
 @Component
